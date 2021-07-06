334d61545332a958c5bf9c913eb919e1f675b536
Falsis <63756985+falsisdev@users.noreply.github.com>
2021-07-06 11:08:42 +0300

    Update api.js

--- api.js
+++ api.js
@@ -11,7 +11,7 @@ if(!req.query.title) {
 }else{
     fetch(`https://api.leref.ga/yt-search?search=${req.query.title}`).then(a => a.json()).then(video => {
         if(video.statusCode === 400){
-            res.json({error: "Böyle bir video bulunamadı."})
+            res.json({error: "No such video was found"})
         }
         res.json({video})
     })
@@ -29,7 +29,7 @@ if(!req.query.title) {
     },
     wiki: async function(req, res) {
         if(!req.query.wiki){
-            res.json({error: "Lütfen bir wiki araması girin.: /wiki?wiki=Batman"})
+            res.json({error: "Cannot find query wiki : /wiki?wiki=Batman"})
         }
         const page = await wiki.page(req.query.wiki);
         const summary = await page.summary();
@@ -38,7 +38,7 @@ if(!req.query.title) {
 },
 color: async function(req, res) {
 if(!req.query.hex) {
-    res.json({error: "Lütfen bir hex kodu girin. Örnek: /color?hex=BDAAF9"})
+    res.json({error: "Cannot find query hex: /color?hex=BDAAF9"})
 }else{
     fetch(`https://www.thecolorapi.com/id?hex=${req.query.hex}&format=png`).then(a => a.json()).then(x => {
         res.json({
@@ -64,24 +64,24 @@ if(!req.query.hex) {
 translate: async function(req, res) {
      if(dvideo.includes(`${req.query.key}`) === true){
     if(!req.query.text){
-        res.json({error: "Lütfen çevirilecek yazıyı giriniz. /translate?text=Hello"})
+        res.json({error: "Cannot find query text : /translate?text=Hello"})
     }else if(!req.query.lang) {
-        res.json({error: "Lütfen çevirilecek dili giriniz. /translate?text=Hello&lang=tr"})
+        res.json({error: "Cannot find query lang: /translate?text=Hello&lang=tr"})
     }
     if(!req.query.from) {
-    res.json({error: "Lütfen hangi dilden çevirileceğini yazın. /translate?text=Hello&lang=tr&from=en"})
+    res.json({error: "Cannot find query from: /translate?text=Hello&lang=tr&from=en"})
     }
    await translate(req.query.text, { from: req.query.from, to: req.query.lang }).then(c => {
    res.json({translated: c.text})
 })
 }else{
-    res.json({error: "Üzgünüm, bu apiyi kullanabilmek için bir api key'e ihtiyacın var. ?key=KEY"})
+    res.json({error: "Cannot find query key : (api key) ?key=KEY"})
 }
 },
     npm: async function (req, res) { 
         if(dvideo.includes(`${req.query.key}`) === true){
         if(!req.query.name) {
-            return("Lütfen bir npm adı belirtin. Örnek: /api/npm?name=falsisdb")
+            return("Cannot find query name: /api/npm?name=falsisdb")
         }
         
         fetch(`https://api.leref.ga/npm?search=${req.query.name}`).then(x => x.json()).then(z => {
@@ -96,7 +96,7 @@ translate: async function(req, res) {
             downloads: z.downloads})
         })
     }else{
-        res.json({hata: "Üzgünüm, bu apiyi kullanabilmek için bir api key'e ihtiyacın var.  ?key=KEY"})
+        res.json({hata: "Cannot find query key : (api key) ?key=KEY"})
     }
       },
       owofy: async function (req, res) {
@@ -115,7 +115,7 @@ translate: async function(req, res) {
           },
             lyrics: async function (req, res) { 
                        if(!req.query.title) return(res.json({
-              hata: "Lütfen bir şarkı adı gir Örnek: /api/lyrics?title=daisy"
+              hata: "Cannot find query title:  /api/lyrics?title=daisy"
             }))
             fetch(`https://api.leref.ga/lyrics?song=${req.query.title}`).then(x => x.json()).then(z => {
               res.json({lyrics: z.lyrics})
